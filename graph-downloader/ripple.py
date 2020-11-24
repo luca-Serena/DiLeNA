2d23b50541fb64a42eda6c9930ff1b78ba17a948
Luca <lucaserena35@gmail.com>
2020-11-24 19:43:26 +0100

    parallel ripple fix

--- graph-downloader/ripple.py
+++ graph-downloader/ripple.py
@@ -1,6 +1,7 @@
 from ripple_api import RippleDataAPIClient
 import multiprocessing as mp
 import sys
+import time
 import datetime
 
 class Transaction:
@@ -12,7 +13,7 @@ class Transaction:
 #Sometimes weird input are received, in case advance 1 second the start time
 def progressStartDate(start):
 	if len (start) > 19:
-		start = start [: -6] # cut +00:00
+		start = start [: -6] 					# cut +00:00
 	oldDate = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S')
 	newDate = oldDate + datetime.timedelta(0,1)
 	return newDate.strftime('%Y-%m-%dT%H:%M:%S')
@@ -36,16 +37,16 @@ def splitInterval (start, end, batches):
 def download (intervals):
 	start = intervals[0]
 	end = intervals[1]
-	avoidLoopsDate = ''
+	previousDate = ''       					    #avoid starvation in case of 100 transactions in the same second
 	while start < end:
-		if avoidLoopsDate == start:
+		if start == previousDate:					#control for starvation
 			start = progressStartDate(start)
-		avoidLoopsDate = start
+		previousDate = start
 		print (start + "   Process: " + str(mp.current_process().pid))
 		params = {"start" : start, "type": "Payment",  "limit" : 100 }
 		query_params = dict(params)
 		txs = api.get_transactions(**query_params)
-		if "transactions" in txs:
+		if "transactions" in txs:					#if not, probably error 429 occurred
 			for t in txs["transactions"]:
 				if (t["tx"]["Account"] != t["tx"]["Destination"] and start < end):
 					nodeSet.add(t["tx"]["Account"])
@@ -56,16 +57,17 @@ def download (intervals):
 					transList.append (Transaction(t["tx"]["Account"], t["tx"]["Destination"], (amount + " " + str(mp.current_process().pid)) ))
 				start = t["date"]
 		else: 
-			start = progressStartDate(start)
+			print ("Too many requests, waiting...")
+			time.sleep (cores * 5)					#wait some time to not overload the server
 
 	return transList, nodeSet
 
 
 api = RippleDataAPIClient('https://data.ripple.com')
 start="2019-01-01T00:00:00"
-end="2019-01-01T00:10:00"
-fileRes = "res/4file.net"
-cores = 4
+end="2019-01-01T01:00:00"
+fileRes = "res/xrp.net"
+cores = 1
 if len(sys.argv) > 2:
 	start = list(sys.argv[1])
 	start[10] = 'T'          	   	#adding 'T' between date and hour as requested for the format
@@ -73,7 +75,7 @@ if len(sys.argv) > 2:
 	end = list(sys.argv[2])
 	end[10] = 'T'
 	end = "".join(end)
-	if len(sys.argv) > 3:      	# file where to store the output
+	if len(sys.argv) > 3:      		# file where to store the output
 		fileRes = sys.argv[3]
 	if len(sys.argv) == 5:			#number of cores to use
 		cores = int(sys.argv[4])
@@ -82,13 +84,12 @@ transList=[]
 nodeSet = set()						#set of nodes. Set data structure helps avoiding repetitions
 nodeDict={}							#it will map incremental int id with addresses
 pool = mp.Pool(processes=cores) 
-dates_pairs = splitInterval (start, end, cores)
-print (dates_pairs)
-parallelRes = pool.map(download, dates_pairs)
+dates_pairs = splitInterval (start, end, cores)			#calculate time intervals to split the workload
+parallelRes = pool.map(download, dates_pairs)			#dowload in parallel
 for batch in parallelRes:
-	for item in batch[1]:
+	for item in batch[1]:								#aggregating nodes
 		nodeSet.add(item)
-	for item in batch[0]:
+	for item in batch[0]:								#aggregating transactions
 		transList.append(item)
 
 with open(fileRes, 'w') as f:	
